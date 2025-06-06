abf5c011c2f09e742f574b49ac261c0743676a18
Jason Catron <jasonacatron@gmail.com>
2025-06-05 20:10:44 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/<your-repo-name>/', // REPLACE THIS with your actual repo name
+  base: '/proactive-roleplaying-notes/',
 })
